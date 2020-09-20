b94b2ce614e73adcb92fecc0a5ca1cc418dd4c14
sooaaib <soaib.safi@gmail.com>
2020-09-20 12:06:49 +0200

    :tada:Initialize front-end (Vue.js)

---
+++ e-vents-front/src/templates/Event.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <h1>{{$page.event.title}}</h1>
+        <img :src="`http://localhost:1337${$page.event.image}`" >
+        <P>{{$page.event.description}}</p>
+    </Layout>
+</template>    
+
+<page-query>
+query{
+  event(id: "2"){
+    id
+    title
+    description
+    duration
+    price
+    date
+    image
+  }
+}
+</page-query>
+
+
+<script>
+export default {
+    metaInfo(){
+        return{
+            title: this.$page.event.title
+        }
+    }
+}
+</script>
